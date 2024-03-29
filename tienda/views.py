870bf5126f2529d5be4ae0a0d22089daf63413e0
matizaidan <leandromatiaszaidan@gmail.com>
2024-03-29 13:51:20 -0300

    Agregando estilos a la lista de productos

--- tienda/views.py
+++ tienda/views.py
@@ -3,9 +3,4 @@ from django.shortcuts import render
 
 def producto_list(request):
     productos = Producto.objects.all()
-
-    # Imprimir los productos en la consola
-    for producto in productos:
-        print(producto.nombre)  # O cualquier otro campo que desees imprimir
-
     return render(request, 'tienda/producto_list.html', {'productos': productos})
