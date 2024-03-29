53c629da8299df1a2b864ac7fd9ab92e66a4e33f
matizaidan <leandromatiaszaidan@gmail.com>
2024-03-29 11:59:38 -0300

    Agregando for producto in productos

--- tienda/views.py
+++ tienda/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
 from .models import Producto
+from django.shortcuts import render
 
 def producto_list(request):
     productos = Producto.objects.all()
-    return render(request, 'tienda/producto_list.html', {'producto': productos})
 
+    # Imprimir los productos en la consola
+    for producto in productos:
+        print(producto.nombre)  # O cualquier otro campo que desees imprimir
+
+    return render(request, 'tienda/producto_list.html', {'productos': productos})
